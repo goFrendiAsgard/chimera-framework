374b64bcf00856ee0556bb22dfcbf537f57959a8
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2017-08-14 16:13:31 +0700

    cck prototype

--- project-template/chains/programs/cck.js
+++ project-template/chains/programs/cck.js
@@ -31,27 +31,79 @@ else{
     let deletionFlagField = config.deletion_flag_field
     let pkField = config.pk_Field
     let validFields = config.valid_fields
-    let modification_field = config.modification_field
-    let modifier_field = config.modifier_field
+    let modificationField = config.modification_field
+    let modifierField = config.modifier_field
 
     let db =  monk(mongoUrl);
 
     if(action == 'get'){
         let criteria = process.length > 4? process.argv[4]: {}
-        db.find(criteria)
+        criteria = preprocessCriteria(deletionFlagField, criteria)
+        db.get(tableName).find(criteria).then((rows) =>{
+            console.log(JSON.stringify(rows))
+        })
     }
     else if(action == 'getOne'){
+        let criteria = process.length > 4? process.argv[4]: {}
+        criteria = preprocessCriteria(deletionFlagField, criteria)
+        db.get(tableName).findOne(criteria).then((rows) =>{
+            console.log(JSON.stringify(rows))
+        })
     }
     else if(action == 'insert'){
+        if(process.argv.length < 6){
+            showUsage()
+        }
+        else{
+            let data = JSON.parse(process.argv[4])
+            let userId = process.argv[5]
+            db.get(tableName).insert(data).then((row) =>{
+                saveHistory(db, historyTableName, data, modificationField, modifierField, userId, ()=>{
+                    console.log(JSON.stringify(row))
+                })
+            })
+        }
     }
     else if(action == 'update'){
+        if(process.argv.length < 7){
+            showUsage()
+        }
+        else{
+            let pkValue = process.argv[4]
+            let data = JSON.parse(process.argv[5])
+            let userId = process.argv[6]
+            db.get(tableName).findOneAndUpdate({pkField:pkValue}, data).then((row) =>{
+                saveHistory(db, historyTableName, data, modificationField, modifierField, userId, ()=>{
+                    console.log(JSON.stringify(row))
+                })
+            })
+        }
     }
     else if(action == 'delete'){
+        if(process.argv.length < 6){
+            showUsage()
+        }
+        else{
+            let pkValue = process.argv[4]
+            let userId = process.argv[5]
+            db.get(tableName).findOneAndUpdate({pkField:pkValue}, {deletionFlagField:1}).then((row) =>{
+                saveHistory(db, historyTableName, data, modificationField, modifierField, userId, ()=>{
+                    console.log(JSON.stringify(row))
+                })
+            })
+        }
     }
     else{
         showUsage()
     }
 
+}
+
+function preprocessCriteria(deletionFlagField, criteria){
+    if(deletionFlagField != ''){
+        criteria = {'$and' : [{deletionFlagField: 0}, criteria]}
+    }
+    return criteria
 }
 
 function showUsage(){
@@ -60,20 +112,19 @@ function showUsage(){
     console.error(' * node '+process.argv[1]+' get [config] [criteria]')
     console.error(' * node '+process.argv[1]+' getOne [config] [pkValue]')
     console.error(' * node '+process.argv[1]+' insert [config] [dataInJSON] [userId]')
-    console.error(' * node '+process.argv[1]+' update [config] [pkValue] [dateInJSON] [userId]')
+    console.error(' * node '+process.argv[1]+' update [config] [pkValue] [dataInJSON] [userId]')
     console.error(' * node '+process.argv[1]+' delete [config] [pkValue] [userId]')
 }
 
-function saveHistory(db, historyTable, data, modificationField, modifierField, userId, callback){
-    let collection = db.get(historyTable)
+function saveHistory(db, historyTableName, data, modificationField, modifierField, userId, callback){
     if(modifierField != ''){
         data[modifierField] = userId
     }
     if(modificationField != ''){
-        data[modificatioNField] = Date.now()
+        data[modificationField] = Date.now()
     }
-    collection.insert([data]).then((docs)=>{
-        callback(docs)
+    db.get(historyTableName).insert([data]).then((row)=>{
+        callback(row)
     })
 
 }
