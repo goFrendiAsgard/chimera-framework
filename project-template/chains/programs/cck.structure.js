9e728305f685dadd65e48f0524a630ee20529cf2
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2017-09-13 22:27:47 +0700

    try to fix bulk insert data

--- project-template/chains/programs/cck.structure.js
+++ project-template/chains/programs/cck.structure.js
@@ -2,24 +2,75 @@
 'use strict';
 
 const db = require('chimera-framework/mongo-driver')
+const async = require('async')
 const structureCollectionName = 'cck.structure'
 
 function createStructure(webConfig, schema, userId, callback){
     let query = {'table':schema.structure.table}
     let structureConfig = db.createDbConfig(webConfig, structureCollectionName, userId)
     let tableConfig = db.createDbConfig(webConfig, schema.structure.table, userId)
-    // altered callback
-    let alteredCallback = function(structure, success, errorMessage){
+    // dataProcessingCallback
+    let dataProcessingCallback = function(structure, success, errorMessage){
         if(success){
             if(Array.isArray(schema.data) && schema.data.length > 0){
-                db.insert(tableConfig, schema.data, function(data, success, errorMessage){
-                    if(success){
-                        callback({'structure':structure, 'data':data}, true, '')
+                // afterDataProcessingCallback
+                let processedData = []
+                let processSuccess = true
+                let processErrorMessage = ''
+                let lastCallback = function(err, result){
+                    callback({'structure':structure, 'data':processedData}, processSuccess, processErrorMessage)
+                }
+                // determine actions based on schema.data
+                let actions = []
+                for(let i=0; i<schema.data.length; i++){
+                    let data = schema.data[i]
+                    // define updateCallback
+                    let updateCallback = function(localCallback){
+                        db.update(tableConfig, data._id, data, function(doc, success, errorMessage){
+                            console.error([doc, success, errorMessage])
+                            if(success){
+                                processedData.push(doc)
+                            }
+                            else{
+                                console.error(errorMessage)
+                                processSuccess = false
+                            }
+                            localCallback()
+                        })
+                    }
+                    // define insertCallback
+                    let insertCallback = function(localCallback){
+                        db.insert(tableConfig, data, function(doc, success, errorMessage){
+                            console.error([doc, success, errorMessage])
+                            if(success){
+                                processedData.push(doc)
+                            }
+                            else{
+                                console.error(errorMessage)
+                                processSuccess = false
+                            }
+                            localCallback()
+                        })
+                    }
+                    // determine which callback to be added to actions
+                    if('_id' in data){
+                        db.find(tableConfig, data._id, function(doc, success, errorMessage){
+                            if(doc != null && '_id' in doc){
+                                actions.push(updateCallback)
+                            }
+                            else{
+                                actions.push(insertCallback)
+                            }
+                        })
                     }
                     else{
-                        callback({'structure':structure, 'data':[]}, false, errorMessage)
+                        actions.push(updateCallback)
                     }
-                })
+                    console.error(actions)
+                }
+                console.error(actions)
+                // run the actions, and then run the lastCallback
+                async.parallel(actions, lastCallback)
             }
             else{
                 callback({'structure':structure, 'data':[]}, true, '')
@@ -36,12 +87,12 @@ function createStructure(webConfig, schema, userId, callback){
         }
         else if(structures.length == 0){
             // the structure doesn't exist
-            db.insert(structureConfig, schema.structure, alteredCallback)
+            db.insert(structureConfig, schema.structure, dataProcessingCallback)
         }
         else{
             let structure = structures[0]
             // the structure exists
-            db.update(structureConfig, structure._id, schema.structure, alteredCallback)
+            db.update(structureConfig, structure._id, schema.structure, dataProcessingCallback)
         }
     })
 }
