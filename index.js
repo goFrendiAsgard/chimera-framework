6d81ea8a1db02e4d53d92c856bb68a28b48bd58f
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2017-09-22 17:04:05 +0700

    starting refactoring, currently on lib/chain.js

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+'use strict';
+
+let cmd = require('./lib/cmd.js')
+let core = require('./lib/core.js')
+let mongoDriver = require('./lib/mongoDriver.js')
+let test = require('./lib/test.js')
+let util = require('./lib/util.js')
+
+// The exported resources
+module.exports = {
+    'executeChain' : core.executeChain,
+    'formatNanoSecond' : util.formatNanoSecond,
+    'deepCopy' : util.deepCopy,
+    'patchObject' : util.patchObject,
+    'eisn' : util.eisn,
+    'cmd' : cmd,
+    'util': util,
+    'test': test,
+    'db' : db,
+}
+
