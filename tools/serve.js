af5c2dcd0e322c6fd91248084001437433ba69d1
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2017-09-24 23:39:58 +0700

    try to refactor web and failed

--- tools/serve.js
+++ tools/serve.js
@@ -2,4 +2,18 @@
 'use strict';
 
 let chimera = require('../index.js')
+let port = 3000
 
+let webConfig = {
+    'routes': {
+        'route' : '/',
+        'method' : 'all',
+        'chain' : '',
+    }
+}
+
+let app = chimera.web.createApp(webConfig)
+
+app.listen(port, function () {
+    console.log('Chimera service started at port ' + port)
+})
