7cb1fcff776e4396724e034d8af56877568f4196
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2017-10-09 10:31:09 +0700

    okay, switch again to 2 spaces and see if it is going to work now

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 let cmd = require('./lib/cmd.js')
 let core = require('./lib/core.js')
 let db = require('./lib/db.js')
-let test = require('./lib/test.js')
 let util = require('./lib/util.js')
 let web = require('./lib/web.js')
 let sender = require('./lib/sender.js')
@@ -13,14 +12,10 @@ let server = require('./lib/server.js')
 // The exported resources
 module.exports = {
   'executeChain': core.executeChain,
-  'run': core.run,
-  'formatNanoSecond': util.formatNanoSecond,
-  'deepCopy': util.deepCopy,
-  'patchObject': util.patchObject,
+  'runChain': core.runChain,
   'eisn': util.eisn,
   'cmd': cmd,
   'util': util,
-  'test': test,
   'db': db,
   'core': core,
   'web': web,
