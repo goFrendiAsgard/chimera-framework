a895dace9f82cbf618038e28dc7c555bef137a54
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2017-10-13 22:25:19 +0700

    delete oldLib

---
+++ test/fractures/java/Substract.java
@@ -0,0 +1,16 @@
+public class Substract
+{
+    // process
+    public static int substract(int n1, int n2)
+    {
+        return n1-n2;
+    }
+
+    // executor
+    public static void main(String[] args)
+    {
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+        System.out.println(substract(n1, n2));
+    }
+}
