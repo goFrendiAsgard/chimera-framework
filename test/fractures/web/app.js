5b5c5b59d05d03d09fd9c5f39da2f91db2532ed4
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2017-10-17 22:26:51 +0700

    create dummy web

--- test/fractures/web/app.js
+++ test/fractures/web/app.js
@@ -2,11 +2,28 @@ const chimera = require('chimera-framework')
 const path = require('path')
 const port = 3010
 const webConfig = {
-  'routes': [{
-    'route': '/hello',
-    'method': 'all',
-    'chain': path.join(__dirname, 'chains/hello.chiml')
-  }],
+  'routes': [
+    {
+      'route': '/hello-string',
+      'method': 'all',
+      'chain': path.join(__dirname, 'chains/hello-string.chiml')
+    },
+    {
+      'route': '/hello-pug',
+      'method': 'all',
+      'chain': path.join(__dirname, 'chains/hello-pug.chiml')
+    },
+    {
+      'route': '/hello-ejs',
+      'method': 'all',
+      'chain': path.join(__dirname, 'chains/hello-ejs.chiml')
+    },
+    {
+      'route': '/hello-json',
+      'method': 'all',
+      'chain': path.join(__dirname, 'chains/hello-json.chiml')
+    },
+  ],
   'staticPath': path.join(__dirname,  'public'),
   'faviconPath': path.join(__dirname, 'public/favicon.ico'),
   'viewPath': path.join(__dirname, 'views'),
