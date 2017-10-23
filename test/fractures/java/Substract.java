90cd783a7d172c3bacc65eba0c461fef569d5e6e
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2017-10-23 20:31:26 +0700

    fix java program

--- test/fractures/java/Substract.java
+++ test/fractures/java/Substract.java
@@ -3,9 +3,10 @@ public class Substract {
     if (args.length < 2) {
       System.out.println("Invalid argument");
       System.out.println("Usage: java Substract <num1> <num2>");
+    } else {
+      int n1 = Integer.parseInt(args[0]);
+      int n2 = Integer.parseInt(args[1]);
+      System.out.println(n1 - n2);
     }
-    int n1 = Integer.parseInt(args[0]);
-    int n2 = Integer.parseInt(args[1]);
-    System.out.println(n1 - n2);
   }
 }
