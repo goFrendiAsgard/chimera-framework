b4317a4c5a696f8ab78fa7d8054a0d6eebbb853f
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2017-11-03 20:36:39 +0700

    rewrite readme

--- example/minastirith/mean.js
+++ example/minastirith/mean.js
@@ -1,9 +1,9 @@
+module.exports = mean
+
 function mean (data) {
   let total = data.reduce((total, num) => { return total + num })
   return parseFloat(total) / data.length
 }
-
-module.exports = mean
 
 if (require.main === module) {
   let data = JSON.parse(process.argv[2])
