6defe958a754c6f6817d6e57a0c532c3e1e11be9
goFrendiAsgard <goFrendiAsgard@gmail.com>
2017-12-15 13:21:10 +0700

    fix web-init tool

--- web/webConfig.default.js
+++ web/webConfig.default.js
@@ -1,17 +1,29 @@
 const path = require('path')
-const middlewares = []
-const mongoUrl = 'mongodb://localhost/chimera-web-app'
-const migrationPath = path.join(__dirname, 'migrations')
 
 const webConfig = {
-  middlewares,
-  mongoUrl,
-  migrationPath,
+  // routes
+  routes: [],
+  // list of express middlewares function
+  middlewares: [],
+  // mongoUrl database
+  mongoUrl: 'mongodb://localhost/chimera-web-app',
+  // verbosity level
   verbose: 0,
+  // automatically do migration to this version if lastVersion is false
+  version: '0',
+  // automatically do migration to the latest version
+  lastVersion: true,
+  // migration path
+  migrationPath: path.join(__dirname, 'migrations'),
+  // startup hook
   startupHook: path.join(__dirname, 'chains/core.startup.chiml'),
+  // location of static resources
   staticPath: path.join(__dirname, 'public'),
+  // favicon path
   faviconPath: path.join(__dirname, 'public/favicon.ico'),
+  // location of view files
   viewPath: path.join(__dirname, 'views'),
+  // error view tempalate
   errorTemplate: path.join(__dirname, 'views/error.ejs')
 }
 
