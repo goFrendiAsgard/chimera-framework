a05bdeabe989e96db6c2d48560e1e6670695bd04
goFrendiAsgard <goFrendiAsgard@gmail.com>
2017-12-15 11:32:01 +0700

    add shim mechanism

--- web/index.js
+++ web/index.js
@@ -1,6 +1,8 @@
 const web = require('chimera-framework/lib/web.js')
+const mongo = require('chimera-framework/lib/mongo.js')
 const port = process.env.PORT || 3000
 
+// load webConfig
 let webConfig
 try {
   console.warn('[INFO] Load webConfig.js')
@@ -8,6 +10,44 @@ try {
 } catch (error) {
   console.warn('[WARNING] webConfig.js is not exist, load webConfig.default.js')
   webConfig = require('./webConfig.default.js')
+}
+
+// define default value of webConfig.vars.$
+if (!('vars' in webConfig)) {
+  webConfig.vars = {}
+}
+if (!('$' in webConfig.vars)) {
+  webConfig.vars.$ = {}
+}
+
+// define migrationCOnfig and cckDbConfig
+webConfig.vars.$.migrationConfig = {
+  mongoUrl: webConfig.mongoUrl,
+  migrationPath: webConfig.migrationPath
+}
+webConfig.vars.$.cckDbConfig = {
+  mongoUrl: webConfig.mongoUrl,
+  collectionName: '_cck'
+}
+
+webConfig.vars.$.createSchema = (config, callback) => {
+  return mongo.execute(webConfig.cckDbConfig, 'insert', config, callback)
+}
+
+webConfig.vars.$.updateSchema = (config, callback) => {
+  let filter = {'name': config.name, 'site': config.site}
+  return mongo.execute(webConfig.cckDbConfig, 'update', filter, config, callback)
+}
+
+webConfig.vars.$.removeSchema = (config, callback) => {
+  return mongo.execute(webConfig.cckDbConfig, 'remove', config, callback)
+}
+
+webConfig.vars.$.getPreprocessedRoutes = (routes, chainCwd) => {
+  for (let i = 0; i < routes.length; i++) {
+    routes[i].chain = chainCwd + routes[i].chain
+  }
+  return routes
 }
 
 let app = web.createApp(webConfig, ...webConfig.middlewares)
--- web/webConfig.default.js
+++ web/webConfig.default.js
@@ -1,51 +1,12 @@
 const path = require('path')
-const mongo = require('chimera-framework/lib/mongo.js')
-const cckCollection = '_cck'
 const middlewares = []
 const mongoUrl = 'mongodb://localhost/chimera-web-app'
 const migrationPath = path.join(__dirname, 'migrations')
-const migrationConfig = {
-  mongoUrl,
-  migrationPath: migrationPath
-}
-const cckDbConfig = {
-  mongoUrl,
-  collectionName: cckCollection
-}
-
-function createSchema (config, callback) {
-  return mongo.execute(cckDbConfig, 'insert', config, callback)
-}
-
-function updateSchema (config, callback) {
-  let filter = {'name': config.name, 'site': config.site}
-  return mongo.execute(cckDbConfig, 'update', filter, config, callback)
-}
-
-function removeSchema (config, callback) {
-  return mongo.execute(cckDbConfig, 'remove', config, callback)
-}
-
-function getPreprocessedRoutes (routes, chainCwd) {
-  for (let i = 0; i < routes.length; i++) {
-    routes[i].chain = chainCwd + routes[i].chain
-  }
-  return routes
-}
-
-const helper = {
-  getPreprocessedRoutes,
-  createSchema,
-  updateSchema,
-  removeSchema
-}
 
 const webConfig = {
-  helper,
   middlewares,
   mongoUrl,
   migrationPath,
-  migrationConfig,
   verbose: 0,
   startupHook: path.join(__dirname, 'chains/core.startup.chiml'),
   staticPath: path.join(__dirname, 'public'),
