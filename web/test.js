d10b5cbbcc3befe0824b98f7ca64c29aa148e254
goFrendiAsgard <goFrendiAsgard@gmail.com>
2017-12-30 14:19:22 +0700

    thinking of using newman before the web app become to big to be tested manually

---
+++ web/test.js
@@ -0,0 +1,10 @@
+var newman = require('newman'); // require newman in your project
+
+// call newman.run to pass `options` object and wait for callback
+newman.run({
+  collection: require('./test-collection.json'),
+  reporters: 'cli'
+}, function (err) {
+  if (err) { throw err; }
+  console.log('collection run complete!');
+});
