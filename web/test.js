5c06bc98c35a0a944365f6cc6755c8951c9c422b
goFrendiAsgard <goFrendiAsgard@gmail.com>
2017-12-30 15:39:09 +0700

    using newman and postman

--- web/test.js
+++ web/test.js
@@ -2,7 +2,7 @@ var newman = require('newman'); // require newman in your project
 
 // call newman.run to pass `options` object and wait for callback
 newman.run({
-  collection: require('./test-collection.json'),
+  collection: require('./test-web.json'),
   reporters: 'cli'
 }, function (err) {
   if (err) { throw err; }
