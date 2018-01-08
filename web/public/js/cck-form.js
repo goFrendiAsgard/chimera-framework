f666f680639f93722b3c55aa0e9db7cf6bdaeca4
goFrendiAsgard <goFrendiAsgard@gmail.com>
2018-01-08 12:11:33 +0700

    add tabs for cck

---
+++ web/public/js/cck-form.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  var tab = $('#form-tabs li.active a').attr('href').slice(1);
+  cwSwitchTab(tab);
+});
+
+$('#form-tabs a.nav-link').click(function (event) {
+  var tab = $(this).attr('href').slice(1);
+  cwSwitchTab(tab);
+  event.preventDefault();
+})
+
+function cwSwitchTab(tab) {
+  $('#form-tabs li').removeClass('active');
+  $('#form-tabs a.nav-link[href="#' + tab + '"]').parent('li').addClass('active');
+  $('*[data-tab]').hide();
+  $('*[data-tab="' + tab + '"], *[data-tab=""]').show();
+}
