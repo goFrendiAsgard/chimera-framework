00eb4595d934de4e66d11fec0aadd23ebae1360c
goFrendiAsgard <goFrendiAsgard@gmail.com>
2018-01-30 22:25:54 +0700

    many2one input prototype

--- web/public/js/cck-form.js
+++ web/public/js/cck-form.js
@@ -44,6 +44,71 @@ function cwSwitchTab (tab) {
   $('*[data-tab="' + tab + '"], *[data-tab=""]').show()
 }
 
+function cwLoadMany2OnePresentationContainer(fieldName, value, ref, keyField, fields) {
+  let q = {}
+  q[keyField] = value
+  $.ajax({
+    url: '/api/v1/' + ref + '?_q=' + JSON.stringify(q),
+    method: 'get',
+    dataType: 'json',
+    success: function (response) {
+      let results = response.results
+      let html = ''
+      if (results.length === 0) {
+        html = value
+      } else {
+        let row = results[0]
+        if (fields.length === 1) {
+          html = row[fields[0]]
+        } else {
+          for (let field of fields) {
+            html += '<b>' + field + ':</b> ' + row[field] + '<br />'
+          }
+        }
+      }
+      $('#' + fieldName + 'PresentationContainer').html(html)
+    }
+  })
+}
+
+function cwLoadMany2OneInputContainer(fieldName, inputContainer, ref, keyField, fields) {
+  let keyword = $('#' + fieldName + 'SearchBox').val()
+  $.ajax({
+    url: '/api/v1/' + ref + '?_k=' + keyword,
+    method: 'get',
+    dataType: 'json',
+    success: function (response) {
+      let results = response.results
+
+      // build the table
+      let html = '<table class="table">'
+
+      // table header
+      html += '<tr>'
+      for (let field of fields) {
+        html += '<th>' + field + '</th>'
+      }
+      html += '<th>Action</th>'
+      html += '</tr>'
+
+      // table content
+      for (let row of results) {
+        html += '<tr>'
+        for (let field of fields) {
+          html += '<td>' + row[field] + '</td>'
+        }
+        html += '<td><a class="' + fieldName + 'BtnSelect btn btn-default" value="' +row[keyField] + '" href="#" data-dismiss="modal">Select</a></td>'
+        html += '</tr>'
+      }
+
+      // end of the table
+      html += '</table>'
+
+      inputContainer.html(html)
+    }
+  })
+}
+
 $(document).ready(function () {
   cwInitAce()
   if ($('#form-tabs li.active a').attr('href')) {
@@ -51,10 +116,9 @@ $(document).ready(function () {
     // don't know, but seems we have to delay this in order to make ace rendered correctly
     setTimeout(function () { cwSwitchTab(tab) }, 50)
   }
-})
-
-$('#form-tabs a.nav-link').click(function (event) {
-  let tab = $(this).attr('href').slice(1)
-  cwSwitchTab(tab)
-  event.preventDefault()
+  $('#form-tabs a.nav-link').click(function (event) {
+    let tab = $(this).attr('href').slice(1)
+    cwSwitchTab(tab)
+    event.preventDefault()
+  })
 })
