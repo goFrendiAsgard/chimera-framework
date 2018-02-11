eccf5e6aa50d64e814af595a793a91e7e19bee96
goFrendiAsgard <goFrendiAsgard@gmail.com>
2018-02-11 09:08:58 +0700

    make simpleplugin simpler

--- web/simpleplugin/info.js
+++
@@ -1,3 +0,0 @@
-console.log('This package is a Chimera CMS plugin')
-console.log('Please type `npm install <this-package-name>` to install')
-console.log('or `npm uninstall <this-package-name>` to uninstall')
