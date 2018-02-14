f1867e44d97d9e1203e66691c906ef7e4d03f40e
goFrendiAsgard <goFrendiAsgard@gmail.com>
2018-02-14 17:44:36 +0700

    try to improve plugin

--- web/plugin.js
+++ web/plugin.js
@@ -4,6 +4,7 @@ const fse = require('fs-extra')
 const path = require('path')
 const async = require('neo-async')
 const {migrate} = require('./migrate.js')
+const {readJsonFile} = require('chimera-framework/lib/util.js')
 
 const specialDirectories = ['chains', 'views', 'public']
 
@@ -16,17 +17,21 @@ function getMigrationPath (pluginName) {
   return path.join(__dirname, 'node_modules', pluginName, 'migrations')
 }
 
-function getSpecialDirectorySrc (pluginName, directory) {
+function getNodeModuleSpecialDirectory (pluginName, directory) {
   return path.join(__dirname, 'node_modules', pluginName, directory)
 }
 
-function getSpecialDirectoryDst (pluginName, directory) {
+function getCmsSpecialDirectory (pluginName, directory) {
   return path.join(__dirname, directory, pluginName)
+}
+
+function getPluginSpecialDirectory (pluginName, directory) {
+  return path.join(__dirname, 'plugins', pluginName, directory)
 }
 
 function install (pluginName) {
   const migrationPath = getMigrationPath(pluginName)
-  copyAllDir(pluginName, (error) => {
+  createCMSDirectory(pluginName, (error) => {
     if (error) {
       return console.error(error)
     }
@@ -36,7 +41,7 @@ function install (pluginName) {
 
 function uninstall (pluginName) {
   const migrationPath = getMigrationPath(pluginName)
-  removeAllDir(pluginName, (error) => {
+  removeCMSDirectory(pluginName, (error) => {
     if (error) {
       return console.error(error)
     }
@@ -44,14 +49,14 @@ function uninstall (pluginName) {
   })
 }
 
-function copyAllDir (pluginName, callback) {
+function createCMSDirectory (pluginName, callback) {
   let actions = []
   for (let directory of specialDirectories) {
-    let src = getSpecialDirectorySrc(pluginName, directory)
+    let src = getNodeModuleSpecialDirectory(pluginName, directory)
     actions.push((next) => {
       fse.pathExists(src, (error, isExist) => {
         if (error || !isExist) { return next (error)}
-        let dst = getSpecialDirectoryDst(pluginName, directory)
+        let dst = getCmsSpecialDirectory(pluginName, directory)
         return fse.copy(src, dst, next)
       })
     })
@@ -59,10 +64,10 @@ function copyAllDir (pluginName, callback) {
   async.parallel(actions, callback)
 }
 
-function removeAllDir (pluginName, callback) {
+function removeCMSDirectory (pluginName, callback) {
   let actions = []
   for (let directory of specialDirectories) {
-    let dst = getSpecialDirectoryDst(pluginName, directory)
+    let dst = getCmsSpecialDirectory(pluginName, directory)
     actions.push((next) => {
       fse.pathExists(dst, (error, isExist) => {
         if (error || !isExist) { return next (error)}
@@ -73,11 +78,67 @@ function removeAllDir (pluginName, callback) {
   async.parallel(actions, callback)
 }
 
+function adjustPackageInfo (pluginName, callback) {
+  const packageFile = path.join(__dirname, 'plugins', pluginName, 'package.json')
+  readJsonFile(packageFile, (error, content) => {
+    if (error) { return callback(error) }
+    try {
+      let obj = JSON.parse(content)
+      let {version} = obj
+      let versionParts = version.split('.')
+      versionParts[versionParts.length - 1] = parseInt(versionParts[versionParts.length - 1]) + 1
+      version = versionParts.join('.')
+      obj.version = version
+      obj.name = pluginName
+      content = JSON.stringify(obj, null, 2)
+      return fse.writeFile(packageFile, content, callback)
+    } catch (error) {
+      return callback(error)
+    }
+  })
+}
+
+function packDirectory (pluginName, callback) {
+
+}
+
+function adjustPackageInfoAndPackDirectory (pluginName, callback) {
+  adjustPackageInfo(pluginName, (error) => {
+    if (error) {
+      return callback(error)
+    }
+    return packDirectory(pluginName, callback)
+  })
+}
+
+function simplePluginCopyFilter (src, dst) {
+  for (let directory of specialDirectories) {
+    if (src.indexOf(directory) > -1) { return false }
+  }
+  return true
+}
+
+function pack (pluginName, callback) {
+  const dst = getPluginSpecialDirectory(pluginName)
+  fse.pathExists(dst, (error, isExist) => {
+    if (!isExist(dst)) {
+      const src = path.join(__dirname, 'plugins', 'simpleplugin')
+      return fse.copy(src, dst, {filter: simplePluginCopyFilter}, (error) => {
+        if (error) { return callback(error) }
+        return adjustPackageInfoAndPackDirectory(pluginName, callback)
+      })
+    }
+    return adjustPackageInfoAndPackDirectory(pluginName, callback)
+  })
+}
+
 if (require.main === module) {
   let action = process.argv.length > 3 ? process.argv[3] : 'install'
   let pluginName = process.argv[2]
   if (action === 'install') {
     install(pluginName)
+  } else if (action === 'pack') {
+    pack(pluginName)
   } else {
     uninstall(pluginName)
   }
