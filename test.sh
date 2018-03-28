bb7c1a86fef55363feb79a49b8064a13ceca8230
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-03-28 20:38:23 +0700

    edit test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 node_modules/.bin/standard --fix && \
 node_modules/.bin/nyc npm test && \
 node_modules/.bin/nyc report --reporter=html && \
-node_modules/.bin/nyc report --reporter lcovonly && \
-node_modules/.bin/codecov -t e2ddf5d0-f0e1-4656-87d0-b84446700c82
+node_modules/.bin/nyc report --reporter lcovonly
