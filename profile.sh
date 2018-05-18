e07a75ddf14b46d880f48f91e0ebd2dda1e71b3c
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-05-18 23:19:43 +0700

    hopefully better performance

---
+++ profile.sh
@@ -0,0 +1,2 @@
+rm isolate-*.log
+node --prof node_modules/mocha/bin/_mocha
--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 node_modules/.bin/standard --fix && \
 node_modules/.bin/nyc npm test && \
 node_modules/.bin/nyc report --reporter=html && \
-node_modules/.bin/nyc report --reporter lcovonly
+node_modules/.bin/nyc report --reporter lcovonly && \
+cp lib/*.js ../chimera-cms/node_modules/chimera-framework/lib/ && \
+node ../chimera-cms/test.js
